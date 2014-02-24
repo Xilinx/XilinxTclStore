b44a9cc5d74f2f51383d2d0c07759c2ac3e591ea
dpefour <dpefour@xilinx.com>
2014-02-24 15:33:43 -0800

    Removed headers

--- tclapp/xilinx/ultrafast/checklist_pkg.tcl
+++ tclapp/xilinx/ultrafast/checklist_pkg.tcl
@@ -1,10 +1,3 @@
-####################################################################################################
-# HEADER_BEGIN
-# COPYRIGHT NOTICE
-# Copyright 2001-2013 Xilinx Inc. All Rights Reserved.
-# http://www.xilinx.com/support
-# HEADER_END
-####################################################################################################
 
 source [file join  [file dirname [info script]] checklist.tcl]
 
