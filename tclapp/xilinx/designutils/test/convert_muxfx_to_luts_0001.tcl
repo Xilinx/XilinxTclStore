a92ce71ca02681ecc4c107349db77b4f566233af
joshg-xlnx <joshg@xilinx.com>
2014-03-19 17:28:50 -0700

    Adding convert_muxfx_to_luts Tcl script to designutils repository

---
+++ tclapp/xilinx/designutils/test/convert_muxfx_to_luts_0001.tcl
@@ -0,0 +1,20 @@
+# Set the File Directory to the current directory location of the script
+set file_dir [file normalize [file dirname [info script]]]
+set unit_test [file rootname [file tail [info script]]]
+
+# Set the Xilinx Tcl App Store Repository to the current repository location
+puts "== Unit Test directory: $file_dir"
+puts "== Unit Test name: $unit_test"
+
+# Set the Name to the name of the script
+set name [file rootname [file tail [info script]]]
+
+# Load the Design Checkpoint for the specific test
+open_checkpoint "$file_dir/src/convert_muxfx_to_luts/$name.dcp"
+
+# Run the write_slr_pblock_xdc script and verify that no error was reported
+if {[catch { ::tclapp::xilinx::designutils::convert_muxfx_to_luts } catchErrorString]} {
+    error [format " -E- Unit test $name failed: %s" $catchErrorString]   
+}
+
+return 0
