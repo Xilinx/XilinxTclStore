331345a01526a9557a31e79096cd3a46d2a429c0
rajklair <rvklair@xilinx.com>
2014-04-11 15:41:03 -0700

    - added more elaboration options for ModelSim

--- tclapp/xilinx/modelsim/register_options.tcl
+++ tclapp/xilinx/modelsim/register_options.tcl
@@ -30,22 +30,23 @@ proc register_options { simulator } {
     return 1
   }
   set options {
-    {{compile.vhdl_syntax}        {enum}   {{93} {93} {{93} {87} {2002} {2008}}}   {Specify VHDL syntax}}
-    {{compile.use_explicit_decl}  {bool}   {1}                                     {Log all signals}}
-    {{compile.log_all_signals}    {bool}   {0}                                     {Log all signals}}
-    {{compile.load_glbl}          {bool}   {1}                                     {Load GLBL module}}
-    {{compile.incremental}        {bool}   {0}                                     {Perform incremental compilation}}
-    {{compile.unifast}            {bool}   {0}                                     {Enable fast simulation models}}
-    {{compile.vlog.more_options}  {string} {}                                      {More VLOG options}}
-    {{compile.vcom.more_options}  {string} {}                                      {More VCOM options}}
-    {{simulate.runtime}           {string} {1000ns}                                {Specify simulation run time}}
-    {{simulate.uut}               {string} {}                                      {Specify instance name for design under test (default:/uut)}}
-    {{simulate.custom_do}         {string} {}                                      {Specify name of the custom do file}}
-    {{simulate.custom_udo}        {string} {}                                      {Specify name of the custom user do file}}
-    {{simulate.sdf_delay}         {enum}   {{sdfmax} {sdfmax} {{sdfmin} {sdfmax}}} {Delay type}}
-    {{simulate.saif}              {string} {}                                      {Specify SAIF file}}
-    {{simulate.64bit}             {bool}   {0}                                     {Call 64bit VSIM compiler}}
-    {{simulate.vsim.more_options} {string} {}                                      {More VSIM options}}
+    {{compile.vhdl_syntax}         {enum}   {{93} {93} {{93} {87} {2002} {2008}}}   {Specify VHDL syntax}}
+    {{compile.use_explicit_decl}   {bool}   {1}                                     {Log all signals}}
+    {{compile.log_all_signals}     {bool}   {0}                                     {Log all signals}}
+    {{compile.load_glbl}           {bool}   {1}                                     {Load GLBL module}}
+    {{compile.incremental}         {bool}   {0}                                     {Perform incremental compilation}}
+    {{compile.unifast}             {bool}   {0}                                     {Enable fast simulation models}}
+    {{compile.vlog.more_options}   {string} {}                                      {More VLOG compilation options}}
+    {{compile.vcom.more_options}   {string} {}                                      {More VCOM compilation options}}
+    {{elaborate.vsim.more_options} {string} {}                                      {More VSIM elaboration options}}
+    {{simulate.runtime}            {string} {1000ns}                                {Specify simulation run time}}
+    {{simulate.uut}                {string} {}                                      {Specify instance name for design under test (default:/uut)}}
+    {{simulate.custom_do}          {string} {}                                      {Specify name of the custom do file}}
+    {{simulate.custom_udo}         {string} {}                                      {Specify name of the custom user do file}}
+    {{simulate.sdf_delay}          {enum}   {{sdfmax} {sdfmax} {{sdfmin} {sdfmax}}} {Delay type}}
+    {{simulate.saif}               {string} {}                                      {Specify SAIF file}}
+    {{simulate.64bit}              {bool}   {0}                                     {Call 64bit VSIM compiler}}
+    {{simulate.vsim.more_options}  {string} {}                                      {More VSIM simulation options}}
   }
   # create options
   ::tclapp::xilinx::modelsim::usf_create_options $simulator $options
