6858e01e2c2c16077ad43c7858d755f1e6e67538
dpefour <dpefour@xilinx.com>
2014-06-12 14:48:34 -0700

    Merged pull request 240 for JUnit. Updated JUnit to 0.2

--- tclapp/xilinx/junit/pkgIndex.tcl
+++ tclapp/xilinx/junit/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::junit 0.1 [list source [file join $dir junit.tcl]]
+package ifneeded ::tclapp::xilinx::junit 0.2 [list source [file join $dir junit.tcl]]
