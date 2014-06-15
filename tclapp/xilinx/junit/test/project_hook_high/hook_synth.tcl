474622dbc8375ab291dfa81263393c8b45a408e2
tclstoreuser <nik.cimino@gmail.com>
2014-06-14 18:09:31 -0600

    fixed junit test

--- tclapp/xilinx/junit/test/project_hook_high/hook_synth.tcl
+++ tclapp/xilinx/junit/test/project_hook_high/hook_synth.tcl
@@ -2,9 +2,15 @@
 
 # prep
 set testDir   [ file normalize [ file dirname [ info script ] ] ]
+set appDir    [ file normalize [ file join $testDir .. .. .. .. ] ]
 set runDir    [ file join $testDir run ]
-puts "= Current Test Dir:\n  $testDir"
-puts "= Current Run Dir:\n  $runDir"
+lappend ::auto_path $appDir
+puts "Using App Dir:\n  $appDir"
+puts "Using Test Dir:\n  $testDir"
+puts "Using Run Dir:\n  $runDir"
+
+package require ::tclapp::xilinx::junit
+namespace import ::tclapp::xilinx::junit::*
 
 set outputReport [ file join $runDir synthReport.xml ]
 set_report $outputReport
