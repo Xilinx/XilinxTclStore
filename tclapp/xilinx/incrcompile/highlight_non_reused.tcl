f82e6cc9776622f817cc3d9d164580f35fd652a5
guideusr <sainath@xilinx.com>
2014-08-27 11:54:08 +0530

    rename arguments to to simple names instead of - keyword

--- tclapp/xilinx/incrcompile/highlight_non_reused.tcl
+++ tclapp/xilinx/incrcompile/highlight_non_reused.tcl
@@ -8,16 +8,16 @@ proc ::tclapp::xilinx::incrcompile::highlight_non_reused { object } {
   # Summary : highlight non reused objects
 
   # Argument Usage:
-  #  object : The object type. The valid values are : -cells -nets -pins -ports -sites
+  #  object : The object type. The valid values are : cells nets pins ports sites
 
   # Return Value:
   # none, objects are highlighted with different colors on GUI
 
   # Categories: xilinxtclstore, incrcompile
 
-  if { $object ne "-cells" && $object ne "-nets" && $object ne "-pins" && $object ne "-ports" && $object ne "-sites" } {
-    puts "Error: Illegal value for argument object, valid values are : -cells -nets -pins -ports -sites"
+  if { $object ne "cells" && $object ne "nets" && $object ne "pins" && $object ne "ports" && $object ne "sites" } {
+    puts "Error: Illegal value for argument object, valid values are : cells nets pins ports sites"
     return
   }
-  highlight_objects -color red [get_non_reused $object]
+  highlight_objects -color red [::tclapp::xilinx::incrcompile::get_non_reused $object]
 }
