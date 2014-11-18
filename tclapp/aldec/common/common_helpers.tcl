0aa3659e13d401d216376384beb0f64b5bb4bc91
bogdans <bogdan.slusarczyk@aldec.com.pl>
2014-11-18 10:16:04 +0100

    errors detection

--- tclapp/aldec/common/common_helpers.tcl
+++ tclapp/aldec/common/common_helpers.tcl
@@ -2462,6 +2462,11 @@ proc usf_check_errors { step results_log_arg } {
         set retval 1
         break
       }
+      if { [regexp -nocase {^Error:.+$} $line] } {
+        set results_log $log
+        set retval 1
+        break
+      }      
     }
   }
   if { $retval } {
