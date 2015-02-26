3f0f70fd688de9c344ff960fb0bba0f93f04edec
binochotta <bin.ochotta@xilinx.com>
2015-02-26 14:15:28 -0800

    bug fixed on 8_19_2014.3

--- tclapp/mycompany/myapp6/myfile1.tcl
+++ tclapp/mycompany/myapp6/myfile1.tcl
@@ -10,6 +10,7 @@ proc ::tclapp::mycompany::myapp6::myproc1 {arg1 {optional1 ,}} {
     # Summary : 
     # A one line summary of what this proc does
     # In branch 8_19_2014
+    # Bug fixes for 9_19_2014 branch 
     
     # Argument Usage:
     # arg1 : A one line summary of this argument
