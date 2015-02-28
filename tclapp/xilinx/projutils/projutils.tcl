56bd040bf61a9dfcef39364e145c719f61ab1520
rajklair <rvklair@xilinx.com>
2015-02-28 12:50:58 -0800

    - support for the -force switch on export_bd_synth

--- tclapp/xilinx/projutils/projutils.tcl
+++ tclapp/xilinx/projutils/projutils.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::projutils {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::projutils 3.19
+package provide ::tclapp::xilinx::projutils 3.20
