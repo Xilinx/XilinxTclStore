02a2f96e3f62332621691cef778d80e7fec96d7d
joshg-xlnx <joshg@xilinx.com>
2015-03-05 12:01:01 -0800

    Adds create_combined_mig_io_design script

---
+++ tclapp/xilinx/designutils/test/create_combined_mig_io_design_0001.tcl
@@ -0,0 +1,40 @@
+# Set the File Directory to the current directory location of the script
+set file_dir [file normalize [file dirname [info script]]]
+set unit_test [file rootname [file tail [info script]]]
+
+# Set the Xilinx Tcl App Store Repository to the current repository location
+puts "== Unit Test directory: $file_dir"
+puts "== Unit Test name: $unit_test"
+
+# Set the Name to the name of the script
+set name [file rootname [file tail [info script]]]
+
+# Create a new Manage IP project
+create_project -ip $name -part xcvu125-flvc2104-2-e-es2 -in_memory
+
+# Create MIG IP instance
+create_ip -name mig -vendor xilinx.com -library ip -module_name mig_0
+
+# Run the Create Combined MIG design script
+if {[catch { ::tclapp::xilinx::designutils::create_combined_mig_io_design -project_name "projtest_$name" -out_dir "." [get_ips mig_0] } catchErrorString]} {
+    close_project
+	file delete -force "./projtest_$name"
+    error [format " -E- Unit test $name failed: %s" $catchErrorString]
+}
+
+# Launch the synthesis run to verify the top module was created correctly
+#launch_runs synth_1
+#wait_on_run synth_1
+
+# Check that the syntehsis completed
+#if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
+#	error error [format " -E- Unit test $name failed: %s" "synth_1 failed"]
+#}
+
+# Close the project
+#close_project
+
+# Delete the project on disk
+file delete -force "./projtest_$name"
+
+return 0
