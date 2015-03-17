4cc59314d811356184339f7e0d9e0dc2a4dc83da
rajklair <rvklair@xilinx.com>
2015-03-17 15:38:09 -0700

    cr:849403 - set PATH to point to Vivado 32-bit libraries for ModelSim/Questa simulation

--- tclapp/xilinx/questa/sim.tcl
+++ tclapp/xilinx/questa/sim.tcl
@@ -997,6 +997,16 @@ proc usf_questa_write_driver_shell_script { do_filename step } {
     }
   } else {
     puts $fh_scr "@echo off"
+    if { ({simulate} == $step) && [get_property 32bit $fs_obj] } {
+      if { [::tclapp::xilinx::questa::usf_is_axi_bfm_ip] } {
+        set simulator_lib [usf_get_simulator_lib_for_bfm]
+        if { {} != $simulator_lib } {
+          set vivado_lib [file normalize [file dirname $simulator_lib]]
+          set vivado_lib [string map {/ \\\\} $vivado_lib]
+          puts $fh_scr "set PATH=$vivado_lib;%PATH%"
+        }
+      }
+    }
     if { {} != $tool_path } {
       puts $fh_scr "set bin_path=$tool_path"
       puts $fh_scr "call %bin_path%/vsim $s_64bit $batch_sw -do \"do \{$do_filename\}\" -l $log_filename"
