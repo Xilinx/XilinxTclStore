96da2a791765708503cf6c9c0345675bf561be99
bogdan-aldec <bogdan.slusarczyk@aldec.com.pl>
2015-07-14 14:35:13 +0200

    several important improvements

--- tclapp/aldec/riviera/register_options.tcl
+++ tclapp/aldec/riviera/register_options.tcl
@@ -8,7 +8,7 @@
 
 package require Vivado 1.2014.1
 
-package require ::tclapp::aldec::common::helpers 1.0
+package require ::tclapp::aldec::common::helpers 1.1
 
 namespace eval ::tclapp::aldec::riviera {
   namespace export register_options
