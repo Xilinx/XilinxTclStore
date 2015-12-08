cb67268caa5062e3d17abefb4a81385e560a743c
bogdan-aldec <bogdan.slusarczyk@aldec.com.pl>
2015-12-08 13:35:14 +0100

    Update 1.3: Excluded files should not appear in the generated scripts.

--- tclapp/aldec/riviera/sim.tcl
+++ tclapp/aldec/riviera/sim.tcl
@@ -8,8 +8,8 @@
 
 package require Vivado 1.2014.1
 
-package require ::tclapp::aldec::common::sim 1.2
-package require ::tclapp::aldec::common::helpers 1.2
+package require ::tclapp::aldec::common::sim 1.3
+package require ::tclapp::aldec::common::helpers 1.3
 
 namespace eval ::tclapp::aldec::riviera {
 
