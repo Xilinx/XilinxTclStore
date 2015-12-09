47ef3465ebf53d21513b91a19a15a4001bb7d8ff
dpefour <dpefour@xilinx.com>
2015-12-09 12:45:12 -0800

    Enhancements to prettyTable (xilinx::designutils)

--- tclapp/xilinx/designutils/pkgIndex.tcl
+++ tclapp/xilinx/designutils/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::designutils 1.25 [list source [file join $dir designutils.tcl]]
+package ifneeded ::tclapp::xilinx::designutils 1.26 [list source [file join $dir designutils.tcl]]
