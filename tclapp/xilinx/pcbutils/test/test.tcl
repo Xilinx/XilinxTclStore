ba99e98d618bd0469297b7a4753b98f99675e8bb
dpefour <dpefour@xilinx.com>
2016-01-13 17:43:59 -0800

    Empty new app pcbutils

---
+++ tclapp/xilinx/pcbutils/test/test.tcl
@@ -0,0 +1,30 @@
+set appName {xilinx::pcbutils}
+  
+set listInstalledApps [::tclapp::list_apps]
+
+set test_dir [file normalize [file dirname [info script]]]
+puts "== Test directory: $test_dir"
+
+set tclapp_repo [file normalize [file join $test_dir .. .. ..]]
+puts "== Application directory: $tclapp_repo"
+
+if {[lsearch -exact $listInstalledApps $appName] != -1} {
+  # Uninstall the app if it is already installed
+  ::tclapp::unload_app $appName
+}
+
+# Install the app and require the package
+catch "package forget ::tclapp::${appName}"
+::tclapp::load_app $appName
+package require ::tclapp::${appName}
+  
+# Start the unit tests
+puts "script is invoked from $test_dir"
+source -notrace [file join $test_dir export_symbol_0001.tcl]
+
+# Uninstall the app if it was not already installed when starting the script
+if {[lsearch -exact $listInstalledApps $appName] == -1} {
+  ::tclapp::unload_app $appName
+}
+
+return 0
