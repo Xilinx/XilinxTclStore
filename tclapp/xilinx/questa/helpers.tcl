1fb2d2e881e99a31b139fb8fde4f78152add5286
tclstoreuser <nik.cimino@gmail.com>
2016-03-23 15:54:09 -0600

    add VCOM VHDL file for XPM simulation, CR:947555

--- tclapp/xilinx/questa/helpers.tcl
+++ tclapp/xilinx/questa/helpers.tcl
@@ -957,11 +957,27 @@ proc usf_get_files_for_compilation_behav_sim { global_files_str_arg } {
       "BlockSrcs"      { set used_in_val "synthesis" }
     }
     set xpm_libraries [get_property -quiet xpm_libraries [current_project]]
+    set b_using_xpm_libraries false
     foreach library $xpm_libraries {
       foreach file [rdi::get_xpm_files -library_name $library] {
         set file_type "SystemVerilog"
         set g_files $global_files_str
         set cmd_str [usf_get_file_cmd_str $file $file_type true $g_files l_incl_dirs_opts]
+        if { {} != $cmd_str } {
+          lappend files $cmd_str
+          lappend l_compile_order_files $file
+          set b_using_xpm_libraries true
+        }
+      }
+    }
+    if { $b_using_xpm_libraries } {
+      set xpm_library [xcs_get_common_xpm_library]
+      set common_xpm_vhdl_files [xcs_get_common_xpm_vhdl_files]
+      foreach file $common_xpm_vhdl_files {
+        set file_type "VHDL"
+        set g_files {}
+        set b_is_xpm true
+        set cmd_str [usf_get_file_cmd_str $file $file_type $b_is_xpm $g_files other_ver_opts $xpm_library]
         if { {} != $cmd_str } {
           lappend files $cmd_str
           lappend l_compile_order_files $file
@@ -1974,7 +1990,7 @@ proc usf_get_global_include_file_cmdstr { incl_files_arg } {
   return [join $file_str " "]
 }
 
-proc usf_get_file_cmd_str { file file_type b_xpm global_files_str l_incl_dirs_opts_arg } {
+proc usf_get_file_cmd_str { file file_type b_xpm global_files_str l_incl_dirs_opts_arg {xpm_library {}} } {
   # Summary:
   # Argument Usage:
   # Return Value:
@@ -2006,6 +2022,10 @@ proc usf_get_file_cmd_str { file file_type b_xpm global_files_str l_incl_dirs_op
           # set file [extract_files -files [list "$file"] -base_dir $dir/ip_files]
         }
       }
+    }
+  } else { ; # File object is not defined. Check if this is an XPM file...
+    if { ($b_xpm) && ([string length $xpm_library] != 0)} {
+      set associated_library $xpm_library
     }
   }
   
