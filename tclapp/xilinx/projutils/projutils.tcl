f55c69ef35352209e1339db740d1befb0e60ce42
rajklair <rvklair@xilinx.com>
2016-03-31 09:04:12 -0700

    948099 - do not re-generate mem files if generated earlier (removed unnecessary call)

--- tclapp/xilinx/projutils/projutils.tcl
+++ tclapp/xilinx/projutils/projutils.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::projutils {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::projutils 3.157
+package provide ::tclapp::xilinx::projutils 3.158
