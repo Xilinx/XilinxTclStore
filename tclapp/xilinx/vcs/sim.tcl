ef61a3d78a824017e396d7e21cf928b4e23ca3fc
rajklair <rvklair@xilinx.com>
2016-05-03 09:52:06 -0700

    - compile glbl into top library for elaborate

--- tclapp/xilinx/vcs/sim.tcl
+++ tclapp/xilinx/vcs/sim.tcl
@@ -728,12 +728,6 @@ proc usf_add_glbl_top_instance { opts_arg top_level_inst_names } {
 
   if { $b_add_glbl } {
     set top_lib [::tclapp::xilinx::vcs::usf_get_top_library]
-    # for post* top_lib is xil_defaultlib for glbl since it is compiled inside netlist
-    if { ({post_synth_sim} == $sim_flow) || ({post_impl_sim} == $sim_flow) } {
-      if { $b_verilog_sim_netlist } {
-        set top_lib "xil_defaultlib"
-      }
-    }
     lappend opts "${top_lib}.glbl"
   }
 }
