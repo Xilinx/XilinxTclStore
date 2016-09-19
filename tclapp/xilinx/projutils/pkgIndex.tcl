4a1d833f6bac6814cff31514ea138f10c4559b75
rajklair <rvklair@xilinx.com>
2016-09-19 15:50:28 -0700

    - removed -single_step switch from export_simulation help

--- tclapp/xilinx/projutils/pkgIndex.tcl
+++ tclapp/xilinx/projutils/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::projutils 3.207 [list source [file join $dir projutils.tcl]]
+package ifneeded ::tclapp::xilinx::projutils 3.208 [list source [file join $dir projutils.tcl]]
