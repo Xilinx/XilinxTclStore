f1d3c627f4c72783042444052520a41b8592b498
rajklair <rvklair@xilinx.com>
2017-05-25 12:01:17 -0700

    - add software build info in script header

--- tclapp/xilinx/modelsim/modelsim.tcl
+++ tclapp/xilinx/modelsim/modelsim.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::modelsim {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::modelsim 2.156
+package provide ::tclapp::xilinx::modelsim 2.157
--- tclapp/xilinx/questa/questa.tcl
+++ tclapp/xilinx/questa/questa.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::questa {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::questa 2.47
+package provide ::tclapp::xilinx::questa 2.48
