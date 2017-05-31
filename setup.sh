7eebf7c1816a072d43bc109aa45fc8df53a04ad5
U-ONSHORE\scott <scott@chewbacca.onshorecs.local>
2017-05-31 13:25:41 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export XILINX_TCLAPP_REPO=e:/XilinxTclStore
+export XILINX_LOCAL_USER_DATA=NO
+export PATH=/cygdrive/e/Xilinx/Vivado/2017.1/bin:$PATH
+export PATH='/cygdrive/c/Program Files/Blue Pearl Software, Inc/Blue Pearl Visual Verification Environment x64':$PATH
+export PATH='c:/Program Files/Blue Pearl Software, Inc/Blue Pearl Visual Verification Environment x64':$PATH
+#export PATH="c:/Program Files/Blue Pearl Software, Inc/Blue Pearl Visual Verification Environment x64":$PATH
+#export PATH="e:/Install.debug/":$PATH
+#xport PATH="/cygdrive/e/Install.debug/":$PATH
