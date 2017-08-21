53ef17bd12fe2f8b60f465496470f4e873b25772
Scott Aron Bloom <scott@onshorecs.local>
2017-08-21 13:43:29 -0700

    Update documentation

--- tclapp/bluepearl/bpsvvs/pkgIndex.tcl
+++ tclapp/bluepearl/bpsvvs/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::bluepearl::bpsvvs 1.0 [list source [file join $dir bpsvvs.tcl]]
+package ifneeded ::tclapp::bluepearl::bpsvvs 1.1 [list source [file join $dir bpsvvs.tcl]]
