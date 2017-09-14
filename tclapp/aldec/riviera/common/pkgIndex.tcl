31e69195bf32ba5005acbe8c1e1277ca486a8349
Bartosz Miera <bartoszmiera@gmail.com>
2017-09-14 14:56:25 +0200

    Generated scripts are now automatically made executable. List of libraries is now included in vlog command.

--- tclapp/aldec/riviera/common/pkgIndex.tcl
+++ tclapp/aldec/riviera/common/pkgIndex.tcl
@@ -8,5 +8,5 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::aldec::common::helpers 1.11 [list source [file join $dir helpers.tcl]]
-package ifneeded ::tclapp::aldec::common::sim 1.11 [list source [file join $dir sim.tcl]]
+package ifneeded ::tclapp::aldec::common::helpers 1.12 [list source [file join $dir helpers.tcl]]
+package ifneeded ::tclapp::aldec::common::sim 1.12 [list source [file join $dir sim.tcl]]
