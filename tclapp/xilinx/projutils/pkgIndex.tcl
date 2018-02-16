d7f7413a20c76628f24efe615d00fa0e1356b49f
Rahul Gupta <rahulg@xilinx.com>
2018-02-17 00:02:13 +0530

    iEnhanced create_rqs_run to take more user option

--- tclapp/xilinx/projutils/pkgIndex.tcl
+++ tclapp/xilinx/projutils/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::projutils 3.313 [list source [file join $dir projutils.tcl]]
+package ifneeded ::tclapp::xilinx::projutils 3.314 [list source [file join $dir projutils.tcl]]
