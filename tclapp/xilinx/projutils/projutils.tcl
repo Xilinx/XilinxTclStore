db3aaf99d1d90890738fb33f48186082f2049b94
rajklair <rvklair@xilinx.com>
2019-03-04 10:24:56 -0800

    - updated warning msg for boost library

--- tclapp/xilinx/projutils/projutils.tcl
+++ tclapp/xilinx/projutils/projutils.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::projutils {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::projutils 3.388
+package provide ::tclapp::xilinx::projutils 3.389
