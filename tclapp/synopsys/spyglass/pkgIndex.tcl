ddc299a1ff5d12e6d6f7eb14ee8d49bec867d71c
Ajay Rana <ajayr@vgintbw359.internal.synopsys.com>
2019-03-04 22:29:50 -0800

    change the app version to 1.6 and company name in small letters

--- tclapp/synopsys/spyglass/pkgIndex.tcl
+++ tclapp/synopsys/spyglass/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::synopsys::spyglass 1.5 [list source [file join $dir spyglass.tcl]]
+package ifneeded ::tclapp::synopsys::spyglass 1.6 [list source [file join $dir spyglass.tcl]]
