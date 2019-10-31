3c8872e7439e90b7ca29d43d2adcecd558f3d13f
rajklair <rvklair@xilinx.com>
2019-10-31 10:52:58 -0700

    - set LIBRARY_PATH for referencing crti.o for ubuntu

--- tclapp/xilinx/xsim/pkgIndex.tcl
+++ tclapp/xilinx/xsim/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::xsim 2.319 [list source [file join $dir xsim.tcl]]
+package ifneeded ::tclapp::xilinx::xsim 2.320 [list source [file join $dir xsim.tcl]]
