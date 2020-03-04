ad67af0f2cb7507ce2011da794a54b604888fec4
Rahul Kunwar <rkunwark@xilinx.com>
2020-03-04 11:18:01 +0530

    Added CSV as non-user-settable filetype.

--- tclapp/xilinx/projutils/write_project_tcl.tcl
+++ tclapp/xilinx/projutils/write_project_tcl.tcl
@@ -190,7 +190,7 @@ variable l_filetype_filter [list]
 # Setup filter for non-user-settable filetypes
 set l_filetype_filter [list "ip" "ipx" "embedded design sources" "elf" "coefficient files" "configuration files" \
                             "block diagrams" "block designs" "dsp design sources" "text" \
-                            "design checkpoint" "waveform configuration file"]
+                            "design checkpoint" "waveform configuration file" "csv"]
 # ip file extension types
 variable l_valid_ip_extns [list]
 set l_valid_ip_extns      [list ".xci" ".bd" ".slx"]
