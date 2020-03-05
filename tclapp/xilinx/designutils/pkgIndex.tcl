85b8c9ad7edf8d9ea46e307471e1cda7b1bbdd94
dpefour <dpefour@xilinx.com>
2020-03-05 14:45:36 -0800

    Added proc report_gtye5_refclk_summary (xilinx::designutils)

--- tclapp/xilinx/designutils/pkgIndex.tcl
+++ tclapp/xilinx/designutils/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::designutils 1.45 [list source [file join $dir designutils.tcl]]
+package ifneeded ::tclapp::xilinx::designutils 1.46 [list source [file join $dir designutils.tcl]]
