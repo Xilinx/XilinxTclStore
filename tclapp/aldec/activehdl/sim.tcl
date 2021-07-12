8204501136e40c9aaefb442ecec4fa21e5114476
RobertKrawczykAldec <robertk@aldec.com.pl>
2021-07-12 10:19:46 +0200

    1.26 SystemC fixes
    SPT80540 - Please remove '-lboost_system' from ccomp command and add -I "<vivado_install>/2021.1/tps/boost_1_72_0 "
    SPT80598 - Split ccomp line with arguments for vivado scripts
    BKO1497 - Remove superfluous -I "/home/tester/Xilinx/Vivado/2021.1/tps/boost_1_72_0" from ccomp in compilation macro
    SPT80579 - Please add 'versal_cips_v3_0_1/xilinx_versal.o' to ccomp command
    SPT80576 - Please add 'xv_cpt_lib_path' path setting in simulation scripts for SystemC designs
    BKO1478 - Add -work switch with default library to addsc command

--- tclapp/aldec/activehdl/sim.tcl
+++ tclapp/aldec/activehdl/sim.tcl
@@ -8,8 +8,8 @@
 
 package require Vivado 1.2014.1
 
-package require ::tclapp::aldec::common::sim 1.25
-package require ::tclapp::aldec::common::helpers 1.25
+package require ::tclapp::aldec::common::sim 1.26
+package require ::tclapp::aldec::common::helpers 1.26
 
 namespace eval ::tclapp::aldec::activehdl {
 
