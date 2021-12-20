bdc14ddbe4b5c5dce0523fb12a07d9bde630ddc2
rajklair <rvklair@xilinx.com>
2021-12-20 10:46:24 -0800

    - throw error message in catch

--- tclapp/xilinx/xsim/pkgIndex.tcl
+++ tclapp/xilinx/xsim/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::xsim 2.422 [list source [file join $dir xsim.tcl]]
+package ifneeded ::tclapp::xilinx::xsim 2.423 [list source [file join $dir xsim.tcl]]
