ad43950de457ec99abba5030c828f9ba549f7fac
rajklair <rvklair@xilinx.com>
2024-08-16 08:13:24 -0700

    - bind noc2 sub-cores

--- tclapp/xilinx/vcs/vcs.tcl
+++ tclapp/xilinx/vcs/vcs.tcl
@@ -9,4 +9,4 @@ namespace eval ::tclapp::xilinx::vcs {
     lappend ::auto_path $home
   }
 }
-package provide ::tclapp::xilinx::vcs 14.1
+package provide ::tclapp::xilinx::vcs 14.2
--- tclapp/xilinx/xcelium/pkgIndex.tcl
+++ tclapp/xilinx/xcelium/pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded ::tclapp::xilinx::xcelium 11.162 [list source [file join $dir xcelium.tcl]]
+package ifneeded ::tclapp::xilinx::xcelium 11.163 [list source [file join $dir xcelium.tcl]]
