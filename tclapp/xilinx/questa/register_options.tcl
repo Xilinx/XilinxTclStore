cbdcc2360530569c2c6f22d9bb7539012fec0dc1
rajklair <rvklair@xilinx.com>
2024-11-15 10:34:46 -0800

    - fixed bug with QIS param

--- tclapp/xilinx/questa/register_options.tcl
+++ tclapp/xilinx/questa/register_options.tcl
@@ -67,7 +67,7 @@ proc register_options { simulator } {
     {{simulate.sc_async_update}    {bool}   {0}                                     {Enable asynchronous request updates for SystemC}}
     {{simulate.vsim.more_options}  {string} {}                                      {More VSIM simulation options}}
   }
-  if { [get_param "simulator.enableqisflow"]) } {
+  if {[get_param "simulator.enableqisflow"]} {
     append {{{elaborate.opt_mode}  {enum}   {{access} {access} {{access} {debug}}}  {Select vopt execution mode (default:access)}}}
   }
   # create options
