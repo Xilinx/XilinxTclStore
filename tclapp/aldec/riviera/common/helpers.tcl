71c0addcddef279f9632909c4376672667de8fa8
RobertKrawczykAldec <49027179+RobertKrawczykAldec@users.noreply.github.com>
2025-03-06 17:51:12 +0100

    SPT83218 - TCL Store updated for Vivado 2025.1 needed
    SPT83218  - TCL Store updated for Vivado 2025.1 needed

--- tclapp/aldec/riviera/common/helpers.tcl
+++ tclapp/aldec/riviera/common/helpers.tcl
@@ -8,7 +8,7 @@
 
 package require Vivado 1.2014.1
 
-package provide ::tclapp::aldec::common::helpers 1.37
+package provide ::tclapp::aldec::common::helpers 1.38
 
 namespace eval ::tclapp::aldec::common {
 
@@ -194,6 +194,15 @@ proc getLibrariesFromLocalRepo {} {
 		set installDir [ join [ lrange $installComps $index end ] "/" ]
 	}
 
+	variable a_sim_lib_info
+	array unset a_sim_lib_info
+
+	variable a_locked_ips
+	array unset a_locked_ips
+
+	variable a_custom_ips
+	array unset a_custom_ips
+
 	set libraryDict [ dict create ]
 	foreach ipObject [get_ips -all -quiet] {
 		if { {} == $ipObject } {
@@ -208,6 +217,14 @@ proc getLibrariesFromLocalRepo {} {
 				}
 
 				dict append libraryDict $lib
+
+				if { ![ info exists a_sim_lib_info($lib) ] } {
+					set a_sim_lib_info($ipObject#$lib) "LOCKED_IP"
+
+					if { ![info exists a_locked_ips($lib)] } {
+						set a_locked_ips($lib) $ipObject
+					}
+				}
 			}
 		} else {
 			set ipDefObject [ get_ipdefs -quiet -all [ get_property -quiet ipdef $ipObject ] ]
@@ -241,12 +258,55 @@ proc getLibrariesFromLocalRepo {} {
 					}
 					
 					dict append libraryDict $lib
+
+					if { ![ info exists a_sim_lib_info($lib) ] } {
+						set a_sim_lib_info($ipObject#$lib) "CUSTOM_IP"
+
+						if { ![ info exists a_custom_ips($lib) ] } {
+							set a_custom_ips($lib) $ipObject
+						}
+					}
 				}
 			}
 		}
 	}
 
 	return [ dict keys $libraryDict ]
+}
+
+proc isLockedIp { _library } {
+	variable a_locked_ips
+
+	if { [ info exists a_locked_ips($_library) ] } {
+		return true
+	}
+
+	return false
+}
+
+proc isCustomIp { _library } {
+	variable a_custom_ips
+
+	if { [ info exists a_custom_ips($_library) ] } {
+		return true
+	}
+
+	return false
+}
+
+proc printIpCompileMessage { _library } {
+	variable a_locked_ips
+	variable a_custom_ips
+
+	set common_txt "source(s) will be compiled locally with the design"
+  
+	if { [ isLockedIp $_library ] } {
+		send_msg_id USF-[usf_aldec_getSimulatorName]-040 INFO "Using sources from the locked IP version (pre-compiled version will not be referenced) - $_library\n"
+	} elseif { [ isCustomIp $_library ] } {
+		send_msg_id USF-[usf_aldec_getSimulatorName]-040 INFO "Using sources from the custom IP version (pre-compiled version will not be referenced) - $_library\n"
+	} else {
+		send_msg_id USF-[usf_aldec_getSimulatorName]-040 INFO "IP version not found from pre-compiled library ($_library) - $common_txt\n"
+	}
 }
 
 proc getDesignLibraries { _files } {
@@ -840,6 +900,8 @@ proc usf_init_vars {} {
 	variable a_sim_cache_lib_info
 	variable a_sim_cache_lib_type_info
 	variable a_shared_library_path_coln
+	variable a_locked_ips
+	variable a_custom_ips
 	
 	variable properties
 	variable generateLaibraryMode
@@ -850,6 +912,8 @@ proc usf_init_vars {} {
 	array unset a_sim_cache_lib_info
 	array unset a_sim_cache_lib_type_info
 	array unset a_shared_library_path_coln
+	array unset a_locked_ips
+	array unset a_custom_ips
 
   set project                      [current_project]
   set properties(project_name)     [get_property "NAME" $project]
@@ -864,6 +928,7 @@ proc usf_init_vars {} {
   set properties(s_simlib_dir)              {}
   set properties(b_int_export_source_files) 0
   set properties(s_gcc_bin_path)            {}
+  set properties(s_sim_version)				{}
   set properties(b_int_compile_glbl)  0
   
   # launch_simulation tcl task args
@@ -4449,7 +4514,8 @@ proc usf_get_source_from_repo { ip_file orig_src_file launch_dir b_is_static_arg
 			} else {
 				if { [lsearch -exact $localDesignLibraries $library] == -1 } {
 					lappend localDesignLibraries $library
-				}
+					printIpCompileMessage $library
+				}	
 			}
 		}
 	
@@ -5501,19 +5567,20 @@ proc usf_get_target_sm_paths { simulator clibs_dir custom_sm_lib_dir b_int_sm_li
 }
 
 proc usf_get_simmodel_dir { simulator type } {
-  # Summary:
-  # Argument Usage:
-  # Return Value:
- 
-  # platform and library extension
+  variable properties
+
   set platform "win64"
   set extn     "dll"
   if {$::tcl_platform(platform) == "unix"} {
     set platform "lnx64"
     set extn "so"
   }
-  # simulator, gcc version, data dir
-  set sim_version [get_param "simulator.${simulator}.version"]
+
+  set sim_version $properties(s_sim_version)
+  if { {} == $sim_version } {
+	set sim_version [get_param "simulator.${simulator}.version"]
+  }
+  
   set gcc_version [get_param "simulator.${simulator}.gcc.version"]
   
   # prefix path
@@ -5928,6 +5995,22 @@ proc isSystemCEnabled {} {
 	}
 
 	return 0
+}
+
+proc usf_find_ip { name } {
+  # Summary:
+  # Argument Usage:
+  # Return Value:
+
+  set null_ip_obj {}
+  foreach ip_obj [get_ips -all -quiet] {
+    set ipdef [get_property -quiet IPDEF $ip_obj]
+    set ip_name [lindex [split $ipdef ":"] 2]
+    if { [string first $name $ip_name] != -1} {
+      return $ip_obj
+    }
+  }
+  return $null_ip_obj
 }
 
 proc usf_get_shared_ip_libraries { clibs_dir } {
