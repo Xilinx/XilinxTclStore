29f0733dc626ea9757c48fdcff487bcffd7cbcb9
RobertKrawczykAldec <49027179+RobertKrawczykAldec@users.noreply.github.com>
2025-07-17 10:54:33 +0200

    BKO2015 - AXI Memory Map transactions recorder for Riviera-PRO
    BKO2015 - AXI Memory Map transactions recorder for Riviera-PRO

--- tclapp/aldec/riviera/sim.tcl
+++ tclapp/aldec/riviera/sim.tcl
@@ -8,8 +8,8 @@
 
 package require Vivado 1.2014.1
 
-package require ::tclapp::aldec::common::sim 1.39
-package require ::tclapp::aldec::common::helpers 1.39
+package require ::tclapp::aldec::common::sim 1.40
+package require ::tclapp::aldec::common::helpers 1.40
 
 namespace eval ::tclapp::aldec::riviera {
 
